6851429ded534206f1b7d7f1d56accf2d1ed4c37
Jesse Pearson <jesse.pearson@automattic.com>
2020-02-06 14:18:29 -0500

    Initial commit

---
+++ hc-wc-account-button.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Happychat WooCommerce Account Button
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  
+// @author       theonetheonlyjesse
+// @require      https://code.jquery.com/jquery-1.12.4.js
+// @match        https://*.happychat.io/*
+// @updateURL    
+// @grant        none
+// ==/UserScript==
+
+var $ = window.jQuery;
+
+// === Detect messages and mark them as SSR ===================================================
+function get_user_id() {
+    $( 'section.user-data-panel__items div.user-panel-items div.user-panel-items__item:first-child div.user-panel-items__item-content' ).each( function( message ) {
+        var $user_id = $(this).text();
+        //console.log( $user_id );
+        $( 'a.hcwcab_wpcom_user_id_button' ).remove();
+        $( 'section.user-data-panel__items div.user-panel-items' ).append( '<a class="user-data-panel__button button hcwcab_wpcom_user_id_button" target="_blank" href="https://woocommerce.com/wp-admin/?wpcom_user_id=' + $user_id + '">WooCommerce User Account</a>' );
+    });
+
+}
+
+// === Execute function on load ===================================================
+$("body").on('DOMSubtreeModified', ".ReactVirtualized__Grid__innerScrollContainer", function() {
+    get_user_id();
+});
+
+window.setInterval(function(){
+  get_user_id();
+}, 5000);
