0e97d14a1224fe62911fb3a667da2bac86cb0859
Jesse Pearson <jesse.pearson@automattic.com>
2020-02-10 06:34:22 -0500

    Cleaned up notes

--- hc-wc-account-button.js
+++ hc-wc-account-button.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HC WC Account Button
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Adds a WC account button to HC
 // @author       theonetheonlyjesse
 // @require      https://code.jquery.com/jquery-1.12.4.js
@@ -12,22 +12,29 @@
 
 var $ = window.jQuery;
 
-// === Detect messages and mark them as SSR ===================================================
+// Where the magic happens. 
 function get_user_id() {
+
+	// Look for the WP user ID.
     $( 'section.user-data-panel__items div.user-panel-items div.user-panel-items__item:first-child div.user-panel-items__item-content' ).each( function( message ) {
-        var $user_id = $(this).text();
-        //console.log( $user_id );
+
+    	// Grab the user ID.
+        var $user_id = $( this ).text();
+        
+        // Since this loops, remove existing button. 
         $( 'a.hcwcab_wpcom_user_id_button' ).remove();
+
+        // Now add the button to access the WC account via WP ID. 
         $( 'section.user-data-panel__items div.user-panel-items' ).append( '<a class="user-data-panel__button button hcwcab_wpcom_user_id_button" target="_blank" href="https://woocommerce.com/wp-admin/?wpcom_user_id=' + $user_id + '">WooCommerce User Account</a>' );
     });
-
 }
 
-// === Execute function on load ===================================================
+// Make the magic happen on load. 
 $("body").on('DOMSubtreeModified', ".ReactVirtualized__Grid__innerScrollContainer", function() {
     get_user_id();
 });
 
-window.setInterval(function(){
+// Wave the magic wand every 5 seconds to make sure the button is up to date. 
+window.setInterval( function() {
   get_user_id();
 }, 5000);
